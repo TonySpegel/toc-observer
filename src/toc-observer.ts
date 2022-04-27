17f064d90561cbe93ec06544aaded9f04757589f
Tony Spegel <tspegel@fc.eah-jena.de>
2022-04-27 22:30:26 +0200

    add documentation

--- src/toc-observer.ts
+++ src/toc-observer.ts
@@ -1,11 +1,4 @@
 /**
- * <toc-observer> highlights the current heading in your
- * table of contents
- *
- * Copyright © 2022 Tony Spegel
- */
-
-/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
@@ -18,31 +11,38 @@ import {
   queryAssignedElements,
 } from 'lit/decorators.js';
 
+
 /**
- * Observes your TOC elements.
- *
+ * <toc-observer> highlights elements in a table of contents 
+ * if their counterparts are visible in the viewport.
+ * 
  * @slot - This element has a slot
+ *
+ * Copyright © 2022 Tony Spegel
  */
 @customElement('toc-observer')
 export class TocObserver extends LitElement {
+  // Converts '_tocList' into a getter that returns the assignedElements of the given slot
+  @queryAssignedElements({slot: 'toc', selector: 'ul'})
+  private _tocList?: Array<HTMLUListElement>;
+  
+  // Identifies the element or document as a reference for interecting items
   @property({type: String})
-  public rootElement = 'null';
-
-  @property({type: Array})
-  public threshold: number[] = [0, 1];
-
+  public rootElement?: string;
+  
+  // Selector for links within '_tocList'
   @property({type: String})
   public tocLinkSelector = '.toc-link';
-
+  
+  // CSS class which is set when observer items are visible
   @property({type: String})
   public tocActiveClass = 'toc-active';
-
+  
+  // Selector for items which will be observed (should be something with an id).
   @property({type: String})
-  public observerItemSelector = 'h2[id], h3[id]';
-
-  @queryAssignedElements({slot: 'toc', selector: 'ul'})
-  private _tocList!: Array<HTMLUListElement>;
-
+  public observerItemSelector = 'section[id]';
+  
+  // Observes any items within your specified 'rootElement' and adds/removes a CSS class
   private observer: IntersectionObserver = new IntersectionObserver(
     (entries: IntersectionObserverEntry[]) => {
       entries.forEach((entry) => {
@@ -58,8 +58,7 @@ export class TocObserver extends LitElement {
     },
     // IntersectionObserver options
     {
-      root: this.ownerDocument.querySelector(this.rootElement),
-      threshold: this.threshold,
+      root: this.ownerDocument.querySelector(this.rootElement!) || null,
     },
   );
   /**
@@ -67,11 +66,15 @@ export class TocObserver extends LitElement {
    * Its id param is obtained by reading the oberver item's target-id.
    */
   private selectTocLink(id: string): HTMLAnchorElement | null {
-    return this._tocList[0].querySelector<HTMLAnchorElement>(
-      `${this.tocLinkSelector}[href="#${id}"]`,
-    );
+    return this._tocList?.length
+      ? this._tocList[0].querySelector<HTMLAnchorElement>(
+          `${this.tocLinkSelector}[href="#${id}"]`,
+        )
+      : null;
   }
-
+  /**
+   * Receive any items within '_tocList' if present or null
+   */
   private get _tocListItems(): HTMLAnchorElement[] | null {
     return this._tocList?.length
       ? Array.from(
@@ -81,17 +84,23 @@ export class TocObserver extends LitElement {
         )
       : null;
   }
-
+  /**
+   * The 'firstUpdated' lifecycle is called after the component's DOM
+   * has been updated the first time, immediately before updated() is called.
+   * Only then an element's slot content (our toc items) is available and can be observed.
+   */
   override firstUpdated(): void {
     const observerItems = this.ownerDocument?.querySelectorAll<HTMLElement>(
       this.observerItemSelector,
     );
-
+    // Observe items when at least one is available
     if (this._tocListItems?.length) {
       observerItems?.forEach((item) => this.observer.observe(item));
     }
   }
-
+  /**
+   * Stop observing when the component is removed from the DOM
+   */
   override disconnectedCallback(): void {
     this.observer.disconnect();
   }
